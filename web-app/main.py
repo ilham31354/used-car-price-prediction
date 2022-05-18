2c05f04278c58326ff1539f9c7e4592c8a341c8e
Ilham Rizanto <34785337+ilham31354@users.noreply.github.com>
2022-05-18 13:51:22 +0700

    Add files via upload

--- web-app/main.py
+++ web-app/main.py
@@ -8,8 +8,13 @@ from PIL import Image
 # loading the saved model
 loaded_model = pickle.load(open("./web-app/model/lr_model.sav", 'rb'))
 
+# Using PMML
+# loaded_model = Model.fromFile('C:/Users/ilham/Notebook/Used car price prediction/Web App/lr_model.pmml')
+
+
 
 # creating a function for Prediction
+
 def used_car_price_prediction_with_sav(input_data):
     # changing the input_data to numpy array
     input_data_as_numpy_array = np.asarray(input_data)
@@ -19,31 +24,6 @@ def used_car_price_prediction_with_sav(input_data):
     prediction = loaded_model.predict(input_data_reshaped)
     print(prediction)
     return f"The price will be around ${round(prediction[0],2)}" 
-
-# def used_car_price_prediction_with_pmml(input_data):
-#     input_data = np.asarray(input_data)
-#     prediction = loaded_model.predict({
-#       "km_driven" : input_data[0],
-#       "mileage" : input_data[1],
-#       "engine" : input_data[2],
-#       "max_power" : input_data[3],
-#       "seats" : input_data[4],
-#       "age" : input_data[5],
-#       "fuel_CNG" : input_data[6],
-#       "fuel_Diesel" : input_data[7],
-#       "fuel_LPG" : input_data[8],
-#       "fuel_Petrol" : input_data[9],
-#       "seller_type_Dealer" : input_data[10],
-#       "seller_type_Individual" : input_data[11],
-#       "transmission_Automatic" : input_data[12],
-#       "transmission_Manual" : input_data[13],
-#       "owner_First.Owner" : input_data[14],
-#       "owner_Fourth...Above.Owner" : input_data[15],
-#       "owner_Second.Owner" : input_data[16],
-#       "owner_Third.Owner" : input_data[17]
-#     })
-#     print(prediction)
-#     return f"The price will be around ${round(prediction[0],2)}"  
     
   
 def main():
@@ -51,14 +31,14 @@ def main():
     st.set_page_config(layout='wide', page_title="INPO MASZEEHHH", page_icon="🚗")
 
     # giving a title
-    header = Image.open('./web-app/images/header.png')
+    header = Image.open('./web-app/images/header2.png')
   
     st.image(header)
     st.title('Used Car Price Prediction Web App')
     
     # getting the input data from the user
         
-    km_driven = st.text_input("KM Driven")
+    km_driven = st.text_input("KM Driven",0)
     mileage = st.slider('Mile Age (Kilometres per Litre)', min_value=10, max_value=40,value=13,step=1)
     engine = st.slider('Engine (CC)', min_value=800, max_value=2700,value=1200,step=100)
     max_power = st.slider('Max Power (Brake Horse Power)', min_value=45, max_value=180,value=80,step=5)
@@ -124,7 +104,6 @@ def main():
     
     if st.button('Price Prediction'):
         price = used_car_price_prediction_with_sav([km_driven, mileage, engine, max_power, seats, age, fuel_CNG, fuel_Diesel, fuel_LPG, seller_type_Dealer, transmission_Automatic, owner1st, owner4th, owner2nd])
-        # price = used_car_price_prediction_with_pmml([km_driven, mileage, engine, max_power, seats, age,fuel_CNG, fuel_Diesel, fuel_LPG, fuel_Petrol, seller_type_Dealer, seller_type_Individual, transmission_Automatic, transmission_Manual, owner1st, owner4th, owner2nd, owner3rd])
         
         
     st.success(price)
