92aedd831b64ce9d048ed833946c3951e7e8829e
Ilham Rizanto <34785337+rizanto@users.noreply.github.com>
2022-05-25 15:42:37 +0700

    Update main.py

--- web-app/main.py
+++ web-app/main.py
@@ -5,7 +5,6 @@ import rpy2.robjects as robjects
 import rpy2.robjects.packages as rpackages
 from rpy2.robjects.vectors import StrVector
 
-RPY2_CFFI_MODE=ABI
 
 # loading the saved model
 randomForest = rpackages.importr('randomForest')
