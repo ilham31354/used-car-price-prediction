ae42d5cc1780fdd59b05d6b3fb0ab6c7f217b74c
Ilham Rizanto <34785337+rizanto@users.noreply.github.com>
2022-05-25 16:10:33 +0700

    Update main.py

--- web-app/main.py
+++ web-app/main.py
@@ -7,8 +7,9 @@ from rpy2.robjects.vectors import StrVector
 
 
 # loading the saved model
-robjects.r('install.packages("randomForest")')
-randomForest = rpackages.importr('randomForest')
+randomForest = robjects.r('install.packages("randomForest")')
+randomForest = robjects.r('library(randomForest)')
+# randomForest = rpackages.importr('randomForest')
 loaded_model = robjects.r('model = readRDS(".//web-app//model//rf_model.rda")')
 
 
